b9ce4726e5d0821d7b2be9f89bc371f26687d877
uditbatra <uditbatra87@gmail.com>
2022-12-20 16:33:53 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello{
+	public static void main(String args[]){
+		
+		int[] numbers = {1,2,3,4,5,6,7,8,9};
+		
+		for(int num : numbers){
+			System.out.println("the number is : " + num);
+		}
+		
+	}	
+}
